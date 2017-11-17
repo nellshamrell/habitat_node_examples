16ab9e8c78ccb74fb537e175b16609a23f53a394
Nell Shamrell <nellshamrell@gmail.com>
2017-11-17 15:37:32 -0800

    initial commit - building blocks of an express app
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

---
+++ misfit_toys/habitat/plan.sh
@@ -0,0 +1,39 @@
+pkg_name=misfit_toys
+pkg_origin=core
+pkg_version="0.1.0"
+pkg_deps=(core/node core/coreutils core/nginx)
+pkg_build_deps=(core/git)
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_unpack() {
+  # Copies all files to $HAB_CACHE_SRC_PATH
+  # TODO: Do not copy unnecessary files
+  pushd "$PLAN_CONTEXT/.." > /dev/null
+  cp -R . "${HAB_CACHE_SRC_PATH}"
+  popd > /dev/null
+}
+
+do_build() {
+  # Install all node modules with npm
+  pushd $HAB_CACHE_SRC_PATH > /dev/null
+  export HOME=$HAB_CACHE_SRC_PATH
+  export PATH=./node_modules/.bin:$PATH
+  npm install
+
+  # Make any binaries executable
+  for b in node_modules/.bin/*; do
+  fix_interpreter $(readlink -f -n $b) core/coreutils bin/env
+  done
+  popd > /dev/null
+}
+
+do_install() {
+	cp -a "${HAB_CACHE_SRC_PATH}" "${pkg_prefix}/app/"
+}
